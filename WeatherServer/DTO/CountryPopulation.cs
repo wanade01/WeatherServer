921bd97e6ba3df969ab61e626830dbc6ebb2de66
wanade01 <wanade01@gmail.com>
2024-03-15 20:48:59 -0700

    Update from 3/15/24

---
+++ WeatherServer/DTO/CountryPopulation.cs
@@ -0,0 +1,9 @@
+﻿namespace WeatherServer.DTO
+{
+    public class CountryPopulation
+    {
+        public required string Name { get; set; }
+        public int CountryId {  get; set; }
+        public int Population {  get; set; }
+    }
+}
