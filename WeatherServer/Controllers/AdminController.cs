42f4bdca4c91711083b128caae93339ede3f828b
wanade01 <wanade01@gmail.com>
2024-04-19 20:45:22 -0700

    update from 4/19/24

--- WeatherServer/Controllers/AdminController.cs
+++ WeatherServer/Controllers/AdminController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using WeatherServer.DTO;
 
 namespace WeatherServer.Controllers
 {
@@ -10,10 +12,31 @@ namespace WeatherServer.Controllers
     public class AdminController(UserManager<WorldCitiesUser> userManager,
         JwtHandler jwtHandler) : ControllerBase
     {
-        [HttpPost]
-        public void Login()
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(LoginRequest loginRequest)
         {
+            WorldCitiesUser? user = await userManager.FindByNameAsync(loginRequest.UserName);
 
+            if (user == null) 
+            {
+                return Unauthorized("Bad Username (I know you're a hacker)");
+            }
+
+            bool success = await userManager.CheckPasswordAsync(user, loginRequest.Password);
+
+            if(!success)
+            {
+                return Unauthorized("Bad password (Now I really know you're a hacker)");
+            }
+
+            JwtSecurityToken token = await jwtHandler.GetTokenAsync(user);
+            string jwtString = new JwtSecurityTokenHandler().WriteToken(token);
+            return Ok(new LoginResult
+            {
+                Success = true,
+                Message = "Mom loves me",
+                Token = jwtString
+            });
         }
     }
 }
